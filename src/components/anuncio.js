4d3a496ddb72dd665f6dddfd9a28885b584f6a14
wesleyvicen <wesley1535@hotmail.com>
2019-11-13 23:12:20 -0300

    bug fixes

--- src/components/anuncio.js
+++ src/components/anuncio.js
@@ -1,53 +1,43 @@
-import  React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
-import "../styles/style.css"
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "../styles/style.css";
 
-class Anuncio extends React.PureComponent{
+class Anuncio extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-    render() {
+    this.state = {
+      index: 0,
+      direction: null
+    };
+  }
 
-        return(
-            <div className={"ad"}>
-            <Carousel>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-                  alt="Third slide"
-                />
+  handleSelect = (selectedIndex, e) => {
+    this.setState({ index: selectedIndex });
+  };
 
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                  alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={"ad"}>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
+  }
 }
 
 export default Anuncio;
--- src/components/data.js
+++ src/components/data.js
@@ -1,19 +1,36 @@
-import * as React from 'react';
-import "../styles/style.css"
+import * as React from "react";
+import "../styles/style.css";
 import * as moment from "moment";
-import 'moment/locale/pt-br';
+import "moment/locale/pt-br";
 
-class Anuncio extends React.PureComponent{
+class Anuncio extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return(
-            <div className={"date"}>
-                {moment().format('Do MMMM YYYY')}
-                <br/>
-                {moment().format('h:mm:ss a')}
-            </div>
-        )
-    }
+    this.state = {
+      tempo: moment().format("Do MMMM YYYY"),
+      current: moment().format("h:mm:ss a")
+    };
+  }
+
+  update() {
+    this.setState(props => ({
+      tempo: moment().format("Do MMMM YYYY"),
+      current: moment().format("h:mm:ss a")
+    }));
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.update(), 1000);
+  }
+  render() {
+    return (
+      <div className={"date"}>
+        {this.state.tempo}
+        <br />
+        {this.state.current}
+      </div>
+    );
+  }
 }
 
 export default Anuncio;
