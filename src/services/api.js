52c4318bfc77f351585600b63f4f2e493adabf84
wesleyvicen <wesley1535@hotmail.com>
2020-02-20 12:16:32 -0300

    arrumado endereço do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://led-marketing-backend.herokuapp.com',
 });
 
 export const apiNoticia = axios.create({
