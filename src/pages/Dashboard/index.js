e00fb8e9035b2cd9a6215491b2af4acaf9e1ca02
wesleyvicen <wesley1535@hotmail.com>
2020-03-27 13:03:54 -0300

    teste 2

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Navbar from '../App/components/Navbar';
-import Map from '../App/';
 class Dashboard extends Component {
   render() {
     return (
       <>
         <Navbar />
-        <Map />
       </>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,6 @@ import Cadastro from './pages/Cadastro';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
-import App from './pages/App';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -30,7 +29,6 @@ const Routes = () => (
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/login" component={SignIn} />
       <Route path="/signup" component={SignUp} />
-      <PrivateRoute path="/app" component={App} />
       <PrivateRoute path="/registry/ad" component={Cadastro} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
