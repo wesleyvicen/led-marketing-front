74cb5b8d7f68b75da0cb1125987552463ed3d70c
wesleyvicen <wesley1535@hotmail.com>
2020-03-28 21:32:06 -0300

    build

---
+++ build/precache-manifest.15b3a410b9d734c0b46e7dee1b254228.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91199c2c5ff9ac59c96e9665331192ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a700c93a1e55517266",
+    "url": "/static/css/2.4267da9e.chunk.css"
+  },
+  {
+    "revision": "c137791268bffa7d7faf",
+    "url": "/static/css/main.6dd5a829.chunk.css"
+  },
+  {
+    "revision": "c3a700c93a1e55517266",
+    "url": "/static/js/2.38a4752a.chunk.js"
+  },
+  {
+    "revision": "c137791268bffa7d7faf",
+    "url": "/static/js/main.5013fddc.chunk.js"
+  },
+  {
+    "revision": "819533d85ac6784fc87b",
+    "url": "/static/js/runtime-main.abcdce2c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3d49f4ca5c55de0afd784c738d57415f",
+    "url": "/static/media/logo.3d49f4ca.png"
+  }
+]);
